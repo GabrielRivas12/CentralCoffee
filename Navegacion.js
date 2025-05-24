ff3d40321a5a1d51fdce3a593d87dbdd760189a6
gabriel <gabrielzero2bi@gmail.com>
2025-05-24 14:03:58 -0600

    Implementacion de drawer

--- Navegacion.js
+++ Navegacion.js
@@ -1,13 +1,13 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
+import Feather from '@expo/vector-icons/Feather';
 
 
 import Ofertas from './src/Screens/Ofertas/Ofertas';
-import IA from './src/Screens/IA/Asistente'
 import Mapa from './src/Screens/Map/Mapa';
-import QR from './src/Screens/QR/QRLista';
-import Perfil from './src/Screens/Perfil/PerfilUsuario'
 import Asistente from './src/Screens/IA/Asistente';
 import QRLista from './src/Screens/QR/QRLista';
 import PerfilUsuario from './src/Screens/Perfil/PerfilUsuario';
@@ -16,8 +16,7 @@ import PerfilUsuario from './src/Screens/Perfil/PerfilUsuario';
 function Navegacion(){
     return (
         <NavigationContainer>
-            <StackNavegacion />
-                
+            <DrawerNavigate />                
           
         </NavigationContainer>
     );
@@ -37,5 +36,49 @@ function Navegacion(){
         </Stack.Navigator>
     )
   }
+
+  const Drawer = createDrawerNavigator();
+
+function DrawerNavigate(){
+    return(
+        <Drawer.Navigator initialRouteName='Ofertas'>
+            <Drawer.Screen name="Ofertas" component={Ofertas} 
+            options={{
+                drawerIcon: ({ color, size }) => (
+                  <Feather name="user" size={15} color={color} />
+                )
+              }}
+            />
+            <Drawer.Screen name="IA" component={Asistente} 
+            options={{
+                drawerIcon: ({ color, size }) => (
+                  <Feather name="file-text" size={15} color={color} />
+                )
+              }}/>
+            <Drawer.Screen name="Mapa" component={Mapa} 
+             options={{
+                drawerIcon: ({ color, size }) => (
+                  <Feather name="layers" size={15} color={color} />
+                )
+              }}/>
+
+             <Drawer.Screen name="QR" component={QRLista} 
+             options={{
+                drawerIcon: ({ color, size }) => (
+                  <Feather name="layers" size={15} color={color} />
+                )
+              }}/>
+               <Drawer.Screen name="PerfilUsuario" component={PerfilUsuario} 
+             options={{
+                drawerIcon: ({ color, size }) => (
+                  <Feather name="layers" size={15} color={color} />
+                )
+              }}/>
+            
+        </Drawer.Navigator>
+    )
+
+}
+
 
 export default Navegacion;
